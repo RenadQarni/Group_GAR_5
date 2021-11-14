e0ed4a58d83a20b549a07ca8d9b0841ac68f9757
FaiAliAlmarwani1 <fai123456789ali@gmail.com>
2021-11-14 21:07:50 +0300

    added two classes
    the client class and User class.

--- src/ProgramPackage/User.java
+++ src/ProgramPackage/User.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ProgramPackage;
 
-/**
- *
- * @author alano
- */
+import java.util.logging.Logger;
+
 public class User {
-    
+
+    private String Usernme;
+    private String Email;
+    private String Phone;
+    private String Password;
+    private static final Logger LOG = Logger.getLogger(User.class.getName());
+
+    public static Logger getLOG() {
+        return LOG;
+    }
+
+    public User(String Usernme, String Email, String Phone, String Password) {
+        this.Usernme = Usernme;
+        this.Email = Email;
+        this.Phone = Phone;
+        this.Password = Password;
+    }
+
+    public void setUsernme(String Usernme) {
+        this.Usernme = Usernme;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setPhone(String Phone) {
+        this.Phone = Phone;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getUsernme() {
+        return Usernme;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void CreateAccount() {
+        //mrthod here
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "Usernme=" + Usernme + ", Email=" + Email + ", Phone=" + Phone + ", Password=" + Password + '}';
+    }
 }
